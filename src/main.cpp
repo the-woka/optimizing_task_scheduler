c4eb6e58c6026ca7ba3e54f9a32a7265ec116a72
the-woka <ois.woka23@gmail.com>
2025-09-29 13:32:53 +0700

    setup project

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << " ";
+    cout << "I am Back" << endl;
+    return 0;
+}
