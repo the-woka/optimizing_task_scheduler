600ef7543527e1b99d15705992b7ad2f77defd88
the-woka <ois.woka23@gmail.com>
2025-09-29 21:25:46 +0700

    setup struct task, generating menus, saving hardcode using vector, add tasks, show list of tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,78 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
+struct Task {
+    int id, duration, deadline, priority;
+};
+
 int main()
 {
-    cout << "Hello World" << " ";
-    cout << "I am Back" << endl;
-    return 0;
+    int choice;
+    vector<Task> tasks;
+
+    while (true)
+    {
+        cout << "Menu: " << endl;
+        cout << "1. Add Task" << endl;
+        cout << "2. Update Task" << endl;
+        cout << "3. Delete Task" << endl;
+        cout << "4. See Tasks" << endl;
+        cout << "5. Optimize Task" << endl;
+        cout << "0. End Program" << endl;
+        cout << "Choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                int n;
+                cout << "Numbers of Data: ";
+                cin >> n;
+
+                for (int i = 0; i < n; i++) {
+                    Task t;
+                    cout << "Task " << i+1 << ": " << endl;
+                    cout << "ID: ";
+                    cin >> t.id;
+                    cout << "Duration (in days): ";
+                    cin >> t.duration;
+                    cout << "Deadline (in days): ";
+                    cin >> t.deadline;
+                    cout << "Priority (in scale 1-3): ";
+                    cin >> t.priority;
+                    tasks.push_back(t);
+                }
+                break;
+            case 2:
+                cout << "Second Menu" << endl;
+                break;
+            case 3:
+                cout << "Third Menu" << endl;
+                break;
+            case 4: {
+                cout << "Data Tasks:" << endl;
+                cout << endl;
+                int i = 1;
+                for (auto &t : tasks) {
+                    cout << "Task " << i++ << endl;
+                    cout << "ID: " << t.id << ", ";
+                    cout << "Duration: " << t.duration << ", ";
+                    cout << "Deadline: " << t.deadline << ", ";
+                    cout << "Priority: " << t.priority << endl;
+                }
+                break;
+            }
+            case 5:
+                cout << "Fifth Menu" << endl;
+                break;
+            case 0:
+                cout << "Task Selesai" << endl;
+                return 0;
+            default:
+                cout << "Wrong Input" << endl;
+                break;
+        }
+    }
 }
